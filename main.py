d0b508f337432c6448482e1621189bb231878799
fortniteFAN999 <115526471+fortniteFAN999@users.noreply.github.com>
2026-06-01 17:24:53 +1000

    created a frame

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import customtkinter as ctk
+
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('green')
+
+class streamApp(ctk.CTk):
+
+    def __init__(self):
+        super().__init__()
+        self.title("Streaming app")
+        self.geometry("720x1080")
+        self.resizable(True, True)
+
+        self._build_ui()
+    
+    def _build_ui(self):
+        self._build_start_frame()
+    
+    def _build_start_frame(self):
+        self.frame_start = ctk.CTkFrame(self)
+
+if __name__ == "__main__":
+    app = streamApp()
+    app.mainloop()
