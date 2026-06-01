cd3acb54743db457ebaa329cd09ccc56ec83796c
fortniteFAN999 <115526471+fortniteFAN999@users.noreply.github.com>
2026-06-01 17:59:11 +1000

    Added a create and edit profile button, changed text size

--- main.py
+++ main.py
@@ -24,25 +24,31 @@ class streamApp(ctk.CTk):
         self.frame_start.grid_rowconfigure((0, 1, 2), weight=1)
 
         #Heading
-        ctk.CTkLabel(self.frame_start, text="Select a profile", font=("Arial", 24)).grid(row=0, column=1, padx=10, pady=10)
+        ctk.CTkLabel(self.frame_start, text="Select Profile", font=("Arial", 30)).grid(row=0, column=1, padx=10, pady=10)
 
         #Menu
-        self.frame_menu = ctk.CTkFrame(master=self.frame_start)
-        self.frame_menu.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
-        self.frame_menu.grid_columnconfigure((0, 1, 2), weight=1)
-        self.frame_menu.grid_rowconfigure((0), weight=1)
+        self.frame_profile_menu = ctk.CTkFrame(master=self.frame_start)
+        self.frame_profile_menu.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
+        self.frame_profile_menu.grid_columnconfigure((0, 1, 2, 3), weight=1)
+        self.frame_profile_menu.grid_rowconfigure((0), weight=1)
 
 
-        #Profile buttons
-        self.button_profile1 = ctk.CTkButton(self.frame_menu, fg_color="#202020", hover_color="#626262", text="Profile 1", width=200, height=200)
-        self.button_profile1.grid(column=0, row=0)
+        #Profile Buttons
+        self.button_profile1 = ctk.CTkButton(self.frame_profile_menu, fg_color="#202020", hover_color="#626262", text="Profile 1", font=("Arial", 24), width=200, height=200)
+        self.button_profile1.grid(row=0, column=0)
 
-        self.button_profile2 = ctk.CTkButton(self.frame_menu, fg_color="#202020", hover_color="#626262", text="Profile 2", width=200, height=200)
-        self.button_profile2.grid(column=1, row=0)
+        self.button_profile2 = ctk.CTkButton(self.frame_profile_menu, fg_color="#202020", hover_color="#626262", text="Profile 2", font=("Arial", 24), width=200, height=200)
+        self.button_profile2.grid(row=0, column=1)
 
-        self.button_profile3 = ctk.CTkButton(self.frame_menu, fg_color="#202020", hover_color="#626262", text="Profile 3", width=200, height=200)
-        self.button_profile3.grid(column=2, row=0)
-        
+        self.button_profile3 = ctk.CTkButton(self.frame_profile_menu, fg_color="#202020", hover_color="#626262", text="Profile 3", font=("Arial", 24), width=200, height=200)
+        self.button_profile3.grid(row=0, column=2)
+
+        self.button_profile_create = ctk.CTkButton(self.frame_profile_menu, fg_color="#1A1A1A", hover_color="#626262", text="Create Profile", font=("Arial", 24), width=200, height=200)
+        self.button_profile_create.grid(row=0, column=3)
+
+        #Edit Profiles Button
+        self.button_edit_profile = ctk.CTkButton(self.frame_start, fg_color="#202020", hover_color="#626262", text="Edit Profiles", font=("Arial", 16), width=200, height=50)
+        self.button_edit_profile.grid(row=2, column=1)
 
 if __name__ == "__main__":
     app = streamApp()
