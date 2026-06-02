b6624c7dd1cfdf12fc71952b79e97c5b671c1146
fortniteFAN999 <115526471+fortniteFAN999@users.noreply.github.com>
2026-06-02 13:02:29 +1000

    Added JSON file for custom themes, started working on the light theme

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import customtkinter as ctk
 from PIL import Image
 
 ctk.set_appearance_mode('light')
-ctk.set_default_color_theme('green')
+ctk.set_default_color_theme("theme_nutflix.JSON")
 
 logo_red = Image.open("images/logo_red.png")
-logo_white = Image.open("images/logo_white.png")
+logo_white = Image.open("images/logo_transparent.png")
 
-class nutflixApp(ctk.CTk):
+class nutflixStart(ctk.CTk):
 
     def __init__(self):
         super().__init__()
@@ -31,7 +31,7 @@ class nutflixApp(ctk.CTk):
         logo = ctk.CTkImage(light_image=logo_white, dark_image=logo_red, size=(30, 30))
         
         #Heading
-        ctk.CTkLabel(self.frame_start, text="Who's Watching?", font=("Arial", 30), text_color="#000000", image=logo).grid(row=0, column=1, padx=10, pady=10)
+        ctk.CTkLabel(self.frame_start, text="Who's Watching?", font=("Arial", 40), text_color="#890000", image=logo).grid(row=0, column=1, padx=10, pady=10)
 
         #Menu
         self.frame_profile_menu = ctk.CTkFrame(master=self.frame_start)
@@ -41,22 +41,22 @@ class nutflixApp(ctk.CTk):
 
 
         #Profile Buttons
-        self.button_profile1 = ctk.CTkButton(self.frame_profile_menu, fg_color="#919191", hover_color="#626262", text="Profile 1", font=("Arial", 24), width=200, height=200)
+        self.button_profile1 = ctk.CTkButton(self.frame_profile_menu, text="Profile 1", font=("Arial", 24), width=200, height=200)
         self.button_profile1.grid(row=0, column=0)
 
-        self.button_profile2 = ctk.CTkButton(self.frame_profile_menu, fg_color="#919191", hover_color="#626262", text="Profile 2", font=("Arial", 24), width=200, height=200)
+        self.button_profile2 = ctk.CTkButton(self.frame_profile_menu, text="Profile 2", font=("Arial", 24), width=200, height=200)
         self.button_profile2.grid(row=0, column=1)
 
-        self.button_profile3 = ctk.CTkButton(self.frame_profile_menu, fg_color="#919191", hover_color="#626262", text="Profile 3", font=("Arial", 24), width=200, height=200)
+        self.button_profile3 = ctk.CTkButton(self.frame_profile_menu, text="Profile 3", font=("Arial", 24), width=200, height=200)
         self.button_profile3.grid(row=0, column=2)
 
-        self.button_profile_create = ctk.CTkButton(self.frame_profile_menu, fg_color="#6D6D6D", hover_color="#626262", text="Create Profile", font=("Arial", 24), width=200, height=200)
+        self.button_profile_create = ctk.CTkButton(self.frame_profile_menu, text="Create Profile", font=("Arial", 24), width=200, height=200)
         self.button_profile_create.grid(row=0, column=3)
 
         #Edit Profiles Button
-        self.button_edit_profile = ctk.CTkButton(self.frame_start, fg_color="#C6C6C6", hover_color="#626262", text="Edit Profiles", font=("Arial", 16), width=200, height=50)
+        self.button_edit_profile = ctk.CTkButton(self.frame_start, text="Edit Profiles", font=("Arial", 16), width=200, height=50)
         self.button_edit_profile.grid(row=2, column=1)
 
 if __name__ == "__main__":
-    app = nutflixApp()
+    app = nutflixStart()
     app.mainloop()
