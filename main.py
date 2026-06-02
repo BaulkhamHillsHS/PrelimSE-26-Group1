fbbbe6f72a76953245a098355bbc9a79e6b3298e
fortniteFAN999 <115526471+fortniteFAN999@users.noreply.github.com>
2026-06-02 12:08:03 +1000

    made ui lighter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 
-ctk.set_appearance_mode('dark')
+ctk.set_appearance_mode('light')
 ctk.set_default_color_theme('green')
 
 class streamApp(ctk.CTk):
@@ -24,7 +24,7 @@ class streamApp(ctk.CTk):
         self.frame_start.grid_rowconfigure((0, 1, 2), weight=1)
 
         #Heading
-        ctk.CTkLabel(self.frame_start, text="Select Profile", font=("Arial", 30)).grid(row=0, column=1, padx=10, pady=10)
+        ctk.CTkLabel(self.frame_start, text="Who's Watching?", font=("Arial", 30), text_color="#000000").grid(row=0, column=1, padx=10, pady=10)
 
         #Menu
         self.frame_profile_menu = ctk.CTkFrame(master=self.frame_start)
@@ -34,20 +34,20 @@ class streamApp(ctk.CTk):
 
 
         #Profile Buttons
-        self.button_profile1 = ctk.CTkButton(self.frame_profile_menu, fg_color="#202020", hover_color="#626262", text="Profile 1", font=("Arial", 24), width=200, height=200)
+        self.button_profile1 = ctk.CTkButton(self.frame_profile_menu, fg_color="#919191", hover_color="#626262", text="Profile 1", font=("Arial", 24), width=200, height=200)
         self.button_profile1.grid(row=0, column=0)
 
-        self.button_profile2 = ctk.CTkButton(self.frame_profile_menu, fg_color="#202020", hover_color="#626262", text="Profile 2", font=("Arial", 24), width=200, height=200)
+        self.button_profile2 = ctk.CTkButton(self.frame_profile_menu, fg_color="#919191", hover_color="#626262", text="Profile 2", font=("Arial", 24), width=200, height=200)
         self.button_profile2.grid(row=0, column=1)
 
-        self.button_profile3 = ctk.CTkButton(self.frame_profile_menu, fg_color="#202020", hover_color="#626262", text="Profile 3", font=("Arial", 24), width=200, height=200)
+        self.button_profile3 = ctk.CTkButton(self.frame_profile_menu, fg_color="#919191", hover_color="#626262", text="Profile 3", font=("Arial", 24), width=200, height=200)
         self.button_profile3.grid(row=0, column=2)
 
-        self.button_profile_create = ctk.CTkButton(self.frame_profile_menu, fg_color="#1A1A1A", hover_color="#626262", text="Create Profile", font=("Arial", 24), width=200, height=200)
+        self.button_profile_create = ctk.CTkButton(self.frame_profile_menu, fg_color="#6D6D6D", hover_color="#626262", text="Create Profile", font=("Arial", 24), width=200, height=200)
         self.button_profile_create.grid(row=0, column=3)
 
         #Edit Profiles Button
-        self.button_edit_profile = ctk.CTkButton(self.frame_start, fg_color="#202020", hover_color="#626262", text="Edit Profiles", font=("Arial", 16), width=200, height=50)
+        self.button_edit_profile = ctk.CTkButton(self.frame_start, fg_color="#C6C6C6", hover_color="#626262", text="Edit Profiles", font=("Arial", 16), width=200, height=50)
         self.button_edit_profile.grid(row=2, column=1)
 
 if __name__ == "__main__":
